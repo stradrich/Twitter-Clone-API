e719c056ad0371ee6217076a79f80324691dc43a
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-06-26 13:13:52 +0800

    Refactor Likes.js code

--- models/Likes.js
+++ models/Likes.js
@@ -1,64 +1,48 @@
-const { Model, DataTypes } = require('sequelize');
+const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db.config');
-const User = require('./user');
-const Tweet = require('./tweet');
 
-class Like extends Model {}
-
-Like.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false
-  },
-  tweet_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: Tweet,
-      key: 'id'
+const Like = sequelize.define(
+  "Like",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      field: "id"
     },
-    onDelete: 'CASCADE',
-  },
-  liked_by: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: User,
-      key: 'id'
+    tweetId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "tweets",
+        key: 'id'
+      },
+      field: "tweet_id"
     },
-    onDelete: 'CASCADE',
+    likedBy: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: 'id'
+      },
+      field: "liked_by"
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: "created_at"
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      field: "updated_at"
+    },
   },
-  created_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-  },
-  updated_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-    onUpdate: 'CASCADE',
-  },
-}, {
-  sequelize,
-  modelName: 'Like',
-  // Enable timestamps
-  timestamps: true,
-  // Don't automatically add createdAt and updatedAt timestamps
-  createdAt: 'created_at',
-  updatedAt: 'updated_at',
-  // Don't use camelcase for automatically added attributes but underscore style
-  // so `updatedAt` will be `updated_at`
-  underscored: true,
-});
-
-User.hasMany(Like, { foreignKey: 'liked_by' });
-Tweet.hasMany(Like, { foreignKey: 'tweet_id' });
-
-Like.belongsTo(User, { foreignKey: 'liked_by' });
-Like.belongsTo(Tweet, { foreignKey: 'tweet_id' });
-
+  {
+    tableName: "likes",
+    timestamps: false
+  }
+)
 
 module.exports = Like;
