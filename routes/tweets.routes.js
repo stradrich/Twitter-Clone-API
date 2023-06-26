f70a33f138685af419bc7a099e9b9056a38e3a9d
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-06-26 22:31:53 +0800

    Rename routes

--- routes/tweets.routes.js
+++ routes/tweets.routes.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const tweetController = require('../controllers/tweets.controller')
 
 // Remove /tweets since the index.js is already using the prefix
-router.post('/tweets', tweetController.createTweet)
+router.post('/', tweetController.createTweet)
 
 router.get('/feed', tweetController.getAllTweets)
 
 // TODO: Change :id into :userId
-router.get('/users/:id/tweets', tweetController.getTweetById)
+router.get('/users/:userId/tweets', tweetController.getTweetById)
 
 // TODO: Implement update and delete tweet.
 module.exports = router;
