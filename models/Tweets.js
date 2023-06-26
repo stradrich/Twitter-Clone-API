160e0b0c0e7b43e95ce8bf2f83d9fb317f9fc1ae
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-06-26 13:27:35 +0800

    Refactor Tweets.js model code

--- models/Tweets.js
+++ models/Tweets.js
@@ -1,50 +1,49 @@
-const { Model, DataTypes } = require('sequelize');
+const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db.config');
-const User = require('./user');
 
-class Tweet extends Model {}
 
-Tweet.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false
-  },
-  tweet_text: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-  },
-  created_by: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: User,
-      key: 'id'
-    }
-  },
-  created_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-  },
-  updated_at: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-    onUpdate: 'CASCADE',
-  },
-}, {
-  sequelize,
-  modelName: 'Tweet',
-  // Enable timestamps
-  timestamps: true,
-  // Don't automatically add createdAt and updatedAt timestamps
-  createdAt: 'created_at',
-  updatedAt: 'updated_at',
-  // Don't use camelcase for automatically added attributes but underscore style
-  // so `updatedAt` will be `updated_at`
-  underscored: true,
-});
+const Tweets = sequelize.define(
+  "Tweets",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      field:'id'
+    },
+    tweet_text: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      field:'tweet_text'
+    },
+    created_by: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id'
+      },
+      field: 'created_by'
 
-module.exports = Tweet;
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+     field: 'created_at'
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      field: 'updated_at'
+    },
+  },
+  {
+    tableName: 'tweets',
+    timestamps: false,
+  }
+)
+
+
+
+
+module.exports = Tweets;
