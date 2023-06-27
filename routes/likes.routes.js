ee96e8c8ce3204a420ce2bc170921a50ccb97633
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-06-27 11:51:38 +0800

    Fix typo

--- routes/likes.routes.js
+++ routes/likes.routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const likesController = require('../controllers/likes.controller')
 
-router.get('./tweets/:id/likes', likesController.getAllLikes)
+router.get('/tweets/:id/likes', likesController.getAllLikes)
 router.post('/tweets/:id/like', likesController.createLike)
 router.delete('/tweet/:id/like/:id', likesController.deleteLike)
 
