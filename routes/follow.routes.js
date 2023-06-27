9517e9d9d0025c35940a0b94613f6075607a88e8
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-06-27 11:22:01 +0800

    Update function name

--- routes/follow.routes.js
+++ routes/follow.routes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const followController = require('../controllers/follow.controller')
 
-router.get('users/:userId/followers', followController.getAllFollowers)
-router.get('users/:userId/following', followController.getAllFollowings)
+router.get('users/:userId/followers', followController.getUserFollowers)
+router.get('users/:userId/following', followController.getUserFollowings)
 router.post('/follow', followController.createFollow)
 router.delete('/follow/:id', followController.deleteFollow)
 
