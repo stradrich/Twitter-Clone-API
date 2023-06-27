ac1f0848d57fb7d43a04367eb7586a4653b6e142
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-06-27 12:49:16 +0800

    Remove route prefix

--- routes/follow.routes.js
+++ routes/follow.routes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const followController = require('../controllers/follow.controller')
 
-router.get('users/:userId/followers', followController.getUserFollowers)
-router.get('users/:userId/following', followController.getUserFollowings)
+router.get('/:userId/followers', followController.getUserFollowers)
+router.get('/:userId/following', followController.getUserFollowings)
 router.post('/follow', followController.createFollow)
 router.delete('/follow/:id', followController.deleteFollow)
 
