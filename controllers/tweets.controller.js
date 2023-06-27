1779c027345e238311b30c2d969be66760400da5
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-06-27 16:11:19 +0800

    Add test code to test implementations

--- controllers/tweets.controller.js
+++ controllers/tweets.controller.js
@@ -1,81 +1,106 @@
-const Tweet = require("../models/Tweets.js")
+const Tweet = require("../models/Tweets.js");
 
 async function getAllTweets(req, res) {
-    try {
-        // Find all tweets (without pagination)
-        const tweets = await Tweet.findAll()
+  try {
+    // Find all tweets (without pagination)
+    const tweets = await Tweet.findAll();
 
-        // Send all tasks as response
-        res.json(tweets)
-    } catch (error) {
-        res.status(500).json({error: error})
-    }
+    // Send all tasks as response
+    res.json(tweets);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 }
 
 async function getTweetById(req, res) {
-    try {
-        // Find tweet by id
-        const tweet = await Tweet.findByPk(parseInt(req.params.id))
+  try {
+    // Find tweet by id
+    const tweet = await Tweet.findByPk(parseInt(req.params.id));
 
-        // Send tweet as response
-        res.json(tweet)
-    } catch (error) {
-        res.status(500).json({error: error})
-    }
+    // Send tweet as response
+    res.json(tweet);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 }
 
 async function createTweet(req, res) {
-    try {
-        // Create tweet using data from req body
-        const tweet = await Tweet.create({
-            ...req.body
-        })
+  try {
+    // Create tweet using data from req body
+    const tweet = await Tweet.create({
+      ...req.body,
+    });
 
-        // Send created tweet as response
-        res.json(tweet)
-    } catch (error) {
-        res.status(500).json({error: error})
-    }
+    // Send created tweet as response
+    res.json(tweet);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 }
 
 async function updateTweet(req, res) {
-    try {
-        const tweet = await Tweet.update(
-            ...req.body,
-            {
-                where: {
-                    id: parseInt(req.params.id)
-                }
-            }
-        )
+  try {
+    // User can only update their own tweets
+    // TESTING
+    const tweet = await Tweet.findByPk(parseInt(req.params.id));
+    console.log("Tweet: ", tweet);
 
-        // Send updated tweet as response
-        res.json(tweet)
-    } catch (error) {
-        res.status(500).json({error: error})
+    if (tweet.createdBy !== req.user.id) {
+        console.log("Cannot update other people's tweet")
     }
+
+    // if (req.body.id !== req.user.id) {
+
+    //   throw 'Cannot update other peoples tweet'
+    // } else {
+    //     const tweet = await Tweet.update(...req.body, {
+    //         where: {
+    //           id: parseInt(req.params.id),
+    //         },
+    //       });
+
+    //       // Send updated tweet as response
+    //       res.json(tweet);
+    // }
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 }
 
 async function deleteTweet(req, res) {
-    try {
-        // Delete tweet by id
-        const tweet = await Tweet.destroy({
-            where: {
-                id: parseInt(req.params.id)
-            }
-        })
+  try {
+    // User can only delete their own tweets
 
-        // Send deleted tweet as response
-        res.json(tweet)
-    } catch (error) {
-        res.status(500).json({error: error})
+    // TESTING
+    const tweet = await Tweet.findByPk(parseInt(req.params.id))
+    console.log('Delete Tweet', tweet)
+
+    if (tweet.createdBy !== req.user.id) {
+        console.log("Cannot delete other people's tweet")
     }
+
+    // if (req.body.id !== req.user.id) {
+    //   throw "Cannot delete other peoples tweet";
+    // } else {
+    //   // Delete tweet by id
+    //   const tweet = await Tweet.destroy({
+    //     where: {
+    //       id: parseInt(req.params.id),
+    //     },
+    //   });
+
+    //   // Send deleted tweet as response
+    //   res.json(tweet);
+    // }
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 }
 
 module.exports = {
-    getAllTweets,
-    getTweetById,
-    createTweet,
-    updateTweet,
-    deleteTweet
-}
+  getAllTweets,
+  getTweetById,
+  createTweet,
+  updateTweet,
+  deleteTweet,
+};
