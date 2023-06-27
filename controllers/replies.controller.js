511ca9025864bf35f1d44872984e2d6641df6176
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-06-27 16:29:31 +0800

    Add test code to test update & reply implementations

--- controllers/replies.controller.js
+++ controllers/replies.controller.js
@@ -7,7 +7,7 @@ async function getReplies(req, res) {
         tweetId: parseInt(req.params.tweetId),
       },
     });
-    
+
     res.json(replies);
   } catch (error) {
     res.status(500).json({ error: error });
@@ -32,13 +32,27 @@ async function createReply(req, res) {
 
 async function updateReply(req, res) {
   try {
-    const reply = await Reply.update(...req.body, {
-      where: {
-        id: parseInt(req.params.id),
-      },
-    });
+    // User can only update their own reply
+    // TESTING
+    const tweet = await Reply.findByPk(parseInt(req.params.id))
+    console.log("Update Reply: ", tweet)
 
-    res.json(reply);
+    if(tweet.createdBy !== req.user.id) {
+      console.log("Cannot update other people's reply")
+    }
+
+    // if (req.body.id !== req.user.id) {
+    //   throw "Cannot update other people's reply"
+    // } else {
+    //   const reply = await Reply.update(...req.body, {
+    //     where: {
+    //       id: parseInt(req.params.id),
+    //     },
+    //   });
+  
+    //   res.json(reply);
+    // }
+
   } catch (error) {
     res.status(500).json({ error: error });
   }
@@ -46,14 +60,29 @@ async function updateReply(req, res) {
 
 async function deleteReply(req, res) {
   try {
-    const reply = await Reply.destroy({
-      where: {
-        id: parseInt(req.params.id),
-      },
-    });
+    // User can only delete their own reply
+    // TESTING
 
-    // Send deleted reply as response
-    res.json(reply);
+    const tweet = await Reply.findByPk(parseInt(req.params.id))
+    console.log("Delete Reply:", tweet)
+
+    if(tweet.createdBy !== req.user.id) {
+      console.log("Cannot delete other people's reply")
+    }
+
+    // if (req.body.id !== req.user.id) {
+    //   throw "Cannot delete other people's reply"
+    // } else {
+    //   const reply = await Reply.destroy({
+    //     where: {
+    //       id: parseInt(req.params.id),
+    //     },
+    //   });
+  
+    //   // Send deleted reply as response
+    //   res.json(reply);
+    // }
+
   } catch (error) {
     res.status(500).json({ error: error });
   }
