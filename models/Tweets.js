891cbb7e7f5d66e8c936962dbbec5080c3538d54
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-06-27 11:07:43 +0800

    Remove redundant lines

--- models/Tweets.js
+++ models/Tweets.js
@@ -1,7 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db.config');
 
-
 const Tweets = sequelize.define(
   "Tweets",
   {
--- routes/replies.routes.js
+++ routes/replies.routes.js
@@ -1,9 +1,3 @@
-// route for get all replies of the tweet id ✅
-// route for create a reply to the tweet id ✅
-// route for update  a reply of the tweet id ✅
-// route for delete reply from the tweet id ✅
-
-
 const express = require('express')
 const router = express.Router()
 const repliesController = require('../controllers/replies.controller')
