477640e61739c79c65c4c51bbeabf805dbd74802
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-06-27 12:49:51 +0800

    Remove route prefix & rename to avoid confusion

--- routes/likes.routes.js
+++ routes/likes.routes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const likesController = require('../controllers/likes.controller')
 
-router.get('/tweets/:id/likes', likesController.getAllLikes)
-router.post('/tweets/:tweetId/like', likesController.createLike)
-router.delete('/tweet/:id/like/:id', likesController.deleteLike)
+router.get('/:id/likes', likesController.getAllLikes)
+router.post('/:tweetId/like', likesController.createLike)
+router.delete('/:tweetId/like/:id', likesController.deleteLike)
 
 module.exports = router;
