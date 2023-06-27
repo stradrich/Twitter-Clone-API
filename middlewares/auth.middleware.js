fcf41f6ac545eea5bdb44c953f1604c1954d4429
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-06-27 16:03:15 +0800

    Add checkRole middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -22,6 +22,19 @@ function verifyToken(req, res, next) {
     return next()
 }
 
+function checkRole(roles) {
+
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)) {
+            return res.status(401).json({error: "Unauthorized"})
+        }
+
+        return next()
+    }
+    
+}
+
 module.exports = {
-    verifyToken
+    verifyToken,
+    checkRole
 }
