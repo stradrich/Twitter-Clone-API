eac2cd7042123be51285b0e8be1c9a58ea48ed3e
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-06-27 12:50:02 +0800

    Remove route prefix

--- routes/replies.routes.js
+++ routes/replies.routes.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 const repliesController = require('../controllers/replies.controller')
 
-router.get('/tweet/:tweetId/reply', repliesController.getReplies)
-router.post('/tweets/:tweetId/reply',repliesController.createReply)
-router.put('/tweet/:tweetId/reply/:id',repliesController.updateReply)
-router.delete('/tweet/:tweetId/reply/:id', repliesController.deleteReply)
+router.get('/:tweetId/reply', repliesController.getReplies)
+router.post('/:tweetId/reply',repliesController.createReply)
+router.put('/:tweetId/reply/:id',repliesController.updateReply)
+router.delete('/:tweetId/reply/:id', repliesController.deleteReply)
 
 module.exports = router;
