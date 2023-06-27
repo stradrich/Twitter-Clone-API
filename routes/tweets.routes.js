36c07df4ea8e0ceb526569c777e72320be5e7304
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-06-27 16:02:29 +0800

    Add verifyToken & checkRole

--- routes/tweets.routes.js
+++ routes/tweets.routes.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const router = express.Router()
 const tweetController = require('../controllers/tweets.controller')
+const { verifyToken, checkRole } = require('../middlewares/auth.middleware')
 
 router.get('/feed', tweetController.getAllTweets)
 router.get('/users/:userId/tweets', tweetController.getTweetById)
-router.post('/', tweetController.createTweet)
-router.put('/:id', tweetController.updateTweet)
-router.delete('/:id', tweetController.deleteTweet)
+
+router.post(
+    '/',
+    verifyToken,
+    checkRole('user', 'admin'),
+    tweetController.createTweet
+)
+
+router.put(
+    '/:tweetId', 
+    verifyToken,
+    checkRole(['user', 'admin']),
+    tweetController.updateTweet
+)
+
+router.delete(
+    '/:tweetId',
+    verifyToken,
+    checkRole(['user', 'admin']),
+    tweetController.deleteTweet
+)
 
 module.exports = router;
