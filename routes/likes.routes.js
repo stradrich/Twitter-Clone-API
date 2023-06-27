b4b554aa6abceff2561501d1a7a4db82d2f0c9f9
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-06-27 16:02:50 +0800

    Add verifyToken & checkRole

--- routes/likes.routes.js
+++ routes/likes.routes.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const likesController = require('../controllers/likes.controller')
+const { verifyToken, checkRole } = require('../middlewares/auth.middleware')
 
 router.get('/:id/likes', likesController.getAllLikes)
-router.post('/:tweetId/like', likesController.createLike)
-router.delete('/:tweetId/like/:id', likesController.deleteLike)
+
+router.post(
+    '/:tweetId/like', 
+    verifyToken,
+    checkRole(['user', 'admin']),
+    likesController.createLike
+)
+
+router.delete(
+    '/:tweetId/like/:likeId',
+    verifyToken,
+    checkRole(['user', 'admin']),
+    likesController.deleteLike
+)
 
 module.exports = router;
