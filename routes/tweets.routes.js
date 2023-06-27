e1c108b2c9bfe5f21c77db844fa9b852ba9e43b9
Amanda Lau <74201433+amaandalau@users.noreply.github.com>
2023-06-27 11:53:20 +0800

    Reorganise code to standardise

--- routes/tweets.routes.js
+++ routes/tweets.routes.js
@@ -2,13 +2,13 @@ const express = require('express')
 const router = express.Router()
 const tweetController = require('../controllers/tweets.controller')
 
-// Remove /tweets since the index.js is already using the prefix
-router.post('/', tweetController.createTweet)
-
 router.get('/feed', tweetController.getAllTweets)
 
 // TODO: Change :id into :userId
 router.get('/users/:userId/tweets', tweetController.getTweetById)
+
+// Remove /tweets since the index.js is already using the prefix
+router.post('/', tweetController.createTweet)
 
 // TODO: Implement update and delete tweet.
 module.exports = router;
