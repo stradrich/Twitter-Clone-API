5dd8f66248a5cc31b98e6c6ea9ff05a0cbf09c3b
ChrisAlan9903 <118709622+ChrisAlan9903@users.noreply.github.com>
2023-06-28 15:45:24 +0800

    updated users controller and tested all users endpoints

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -16,7 +16,7 @@ async function getAllUsers(req, res) {
 async function getUserById(req, res) {
   try {
     // Find user by id
-    const user = await User.findByPk(parseInt(req.params.id));
+    const user = await User.findByPk(parseInt(req.params.userId));
 
     res.json(user);
   } catch (error) {
@@ -33,8 +33,11 @@ async function createUser(req, res) {
       throw ' Unauthorized'
     } 
 
+    const hashedPassword = hashPassword(req.body.password);
+
     const user = await User.create({
       ...req.body,
+      password: hashedPassword,
     });
     res.json(user);
   } catch (error) {
@@ -47,32 +50,28 @@ async function updateUser(req, res) {
     // User can only update their own profile
     // TESTING
 
-    const user = await User.findByPk(parseInt(req.params.id))
+    const user = await User.findByPk(parseInt(req.params.userId))
     console.log("Update User", user)
 
     if(user.id !== req.user.id) {
-      console.log("Cannot update other people's profile")
-    }
-
-    // if(req.body.id !== req.user.id) {
-    //   throw "Cannot update other people's profile"
-    // } else {
-    //   const hashedPassword = hashPassword(req.body.password);
-    //   const user = await User.update(
-    //     {
-    //       ...req.body,
-    //       password: hashedPassword,
-    //     },
-    //     {
-    //       where: {
-    //         id: parseInt(req.params.id),
-    //       },
-    //     }
-    //   );
+     throw "Cannot update other people's profile"
+    } else {
+      const hashedPassword = hashPassword(req.body.password);
+      const updatedUser = await User.update(
+        {
+          ...req.body,
+          password: hashedPassword,
+        },
+        {
+          where: {
+            id: parseInt(req.params.userId),
+          },
+        }
+      );
   
-    //   // Send updated user as response
-    //   res.json(user);
-    // }
+      // Send updated updatedUser as response
+      res.json(updatedUser);
+    }
 
   } catch (error) {
     res.status(500).json({ error: error });
@@ -84,24 +83,22 @@ async function deleteUser(req, res) {
     // User can only delete their own profile
     // TESTING
 
-    const user = await User.findByPk(parseInt(req.params.id))
+    const user = await User.findByPk(parseInt(req.params.userId))
     console.log("Delete User: ", user)
+    console.log(`req role: `, req.user.role );
+    console.log(`params role: `, user.role );
 
-    if(user.id !== req.user.id) {
-      console.log("Cannot delete other people's profile")
+    if(req.user.role !== 'admin' && user.id !== req.user.id) {
+      throw "Cannot delete other people's profile"
+    } else {
+      const user = await User.destroy({
+        where: {
+          id: parseInt(req.params.userId),
+        },
+      });
+      // Send deleted user as response
+      res.json(user);
     }
-
-    // if (req.body.id !== req.user.id) {
-    //   throw "Cannot delete other people"
-    // } else {
-    //   const user = await User.destroy({
-    //     where: {
-    //       id: parseInt(req.params.id),
-    //     },
-    //   });
-    //   // Send deleted user as response
-    //   res.json(user);
-    // }
 
   } catch (error) {
     res.status(500).json({ error: error });
